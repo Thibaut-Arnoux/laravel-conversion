3693cdcc8ae70a3e7e12c24ad6a723bbbd3ff2d5
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-09-16 20:13:53 +0200

    [File] : Resource and Request upload

---
+++ app/Http/Resources/FileResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin \App\Models\File
+ */
+class FileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array{id: string, name: string, extension: string, created_at: Carbon|null, updated_at: Carbon|null}
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'extension' => $this->extension,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
