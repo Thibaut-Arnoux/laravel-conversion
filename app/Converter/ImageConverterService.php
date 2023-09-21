1807fec8bb8da2fc11443d85fa869122a59d5a94
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-09-21 18:36:58 +0200

    [ImageConversion] : Add some docs

--- app/Converter/ImageConverterService.php
+++ app/Converter/ImageConverterService.php
@@ -12,22 +12,34 @@ use Storage;
 
 class ImageConverterService implements IConverterService
 {
+    /**
+     * Converts a file to a specified extension and saves it.
+     *
+     * @param  File  $file The file to be converted.
+     * @param  FileExtensionEnum  $convertExtension The extension to convert the file to.
+     * @return Conversion The conversion object that represents the converted file.
+     *
+     * @throws Exception When no conversion is required for the given extension.
+     */
     public function convert(File $file, FileExtensionEnum $convertExtension): Conversion
     {
+        // prepare paths
         $temporaryDirectory = (new TemporaryDirectory())->create();
         $convertPath = $temporaryDirectory->path($file->name.'.'.$convertExtension->value);
 
+        // convert file
         match ($convertExtension->value) {
             'jpg', 'jpeg', 'png' => throw new Exception('No conversion required'),
             'pdf' => $this->toPdf(Storage::path($file->path), $convertPath),
         };
 
+        // save converted file on disk
         $convertHttpFile = new HttpFile($convertPath);
         $convertHashPath = $convertHttpFile->hashName();
         Storage::putFileAs('', $convertHttpFile, $convertHashPath);
         $temporaryDirectory->delete();
 
-        // register convert file
+        // save converted file on db
         // TODO : Commit file only when Conversion is saved
         $convertFile = new File();
         $convertFile->name = $file->name;
@@ -35,7 +47,7 @@ class ImageConverterService implements IConverterService
         $convertFile->extension = $convertExtension;
         $convertFile->save();
 
-        // register conversion
+        // save conversion
         $conversion = new Conversion();
         $conversion->originalFile()->associate($file);
         $conversion->convertFile()->associate($convertFile);
@@ -44,6 +56,14 @@ class ImageConverterService implements IConverterService
         return $conversion;
     }
 
+    /**
+     * Converts an image file to PDF format.
+     *
+     * @param  string  $inputPath The path to the input image file.
+     * @param  string  $outputPath The path to save the output PDF file.
+     *
+     * @throws Exception If there is an error during the conversion process.
+     */
     private function toPdf(string $inputPath, string $outputPath): void
     {
         try {
@@ -51,7 +71,7 @@ class ImageConverterService implements IConverterService
             $pdf->setImageFormat('pdf');
             $pdf->writeImage($outputPath);
         } catch (Exception $e) {
-            throw new Exception($e->getMessage());
+            throw new Exception('Failed to convert img to pdf: '.$e->getMessage());
         }
     }
 }
