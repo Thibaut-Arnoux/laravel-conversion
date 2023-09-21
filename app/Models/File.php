6c283fa547d83eb3d73009e73f87ed7092a9f3a1
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-09-21 18:38:04 +0200

    [File] : Add conversions relation on original file

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,6 +6,7 @@ use App\Enums\FileExtensionEnum;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class File extends Model
@@ -17,4 +18,14 @@ class File extends Model
     protected $casts = [
         'extension' => FileExtensionEnum::class,
     ];
+
+    /**
+     * Retrieves the conversions associated with this model.
+     *
+     * @return HasMany<Conversion>
+     */
+    public function conversions(): HasMany
+    {
+        return $this->hasMany(Conversion::class, 'original_file_id');
+    }
 }
