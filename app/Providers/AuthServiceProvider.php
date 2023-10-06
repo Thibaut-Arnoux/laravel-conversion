37bbbb6a77b655bc3067b20f99d32b77757c3ad2
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-10-06 16:48:23 +0200

    [Env/Front] : Add env front-end url to redirect email forgot button

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 
 use App\Models\File;
+use App\Models\User;
 use App\Policies\FilePolicy;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,6 +26,21 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $frontEndUrl = env('FRONTEND_URL', 'http://localhost');
+        $this->setFrontEndUrlInResetPasswordEmail($frontEndUrl);
+    }
+
+    protected function setFrontEndUrlInResetPasswordEmail(string $frontEndUrl = ''): void
+    {
+        // update url in ResetPassword Email to frontend url
+        ResetPassword::createUrlUsing(function (User $notifiable, string $token) {
+            // TODO : should be update with front end url
+            // return $frontEndUrl.'/api/auth/password/email/reset?token='.$token;
+
+            return url(route('auth.password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+        });
     }
 }
